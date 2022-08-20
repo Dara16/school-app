c53f5120bfd1f42ed97b421501ffc2fd36ce05d2
Dara16 <just4oludee@yahoo.com>
2022-08-19 22:40:07 -0400

    confirmation and password columns added to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,27 @@
 class User < ApplicationRecord
+    CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
+
     before_save :downcase_email
 
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, presence: true
 
     has_secure_password
+
+    def confirm!
+        update_columns(confirmed_at: Time.now)
+    end
+
+    def confirmed?
+        confirmed_at.present?
+    end
+
+    def generate_confirmation_token
+        signed_id expires_in: CONFIRMATION_TOKEN_EXPIRATION, purpose: :confirm_email
+    end
+
+    def unconfirmed?
+        !confirmed?
+    end
 
     private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_19_232522) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_003815) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
